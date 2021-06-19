f529cad714a9d500440500e748ad9e44185cf078
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-19 13:22:01 -0500

    feat added basic README with expected behavior

---
+++ README.md
@@ -0,0 +1,17 @@
+# Debug Microservice
+
+## Behaviour
+
+- Crear mensajes de error.
+- Almacenarlos en un archivo de texto.
+- Poder crear diferentes archivos de texto para diferentes contextos de errores.
+- Obtener todos los registros de errores de un archivo.
+- Poder descargar el archivo de errores.
+
+## End-Points
+
+Crear archivos de error y mensajes de error en ese archivo.
+
+Descargar un archivo de error.
+
+Obtener todos los registro de error de una archivo en especifico.
--- src/main/java/com/pablohdz/debug/infra/CreateMessageController.java
+++ src/main/java/com/pablohdz/debug/infra/CreateMessageController.java
@@ -4,10 +4,17 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class CreateMessageController implements HttpHandler {
+    // TODO: Read data Json to Request
+    // TODO: Create file if not exists
+    // TODO: 6/19/21 append the error in the file
+    // TODO: 6/19/21 Response with ok is all successful
+
     @Override
     public void handle(HttpExchange exchange) {
         System.out.println(exchange.getRequestMethod());
         System.out.println(exchange.getRequestHeaders());
         System.out.println(exchange.getResponseBody());
     }
+
+
 }
