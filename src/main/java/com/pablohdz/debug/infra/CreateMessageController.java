44ad6640c131394295154d877ff9b1ece9afca1a
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-20 18:09:48 -0500

    feat can read the data from JSON

--- src/main/java/com/pablohdz/debug/infra/CreateMessageController.java
+++ src/main/java/com/pablohdz/debug/infra/CreateMessageController.java
@@ -3,8 +3,12 @@ package com.pablohdz.debug.infra;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class CreateMessageController implements HttpHandler {
-    // TODO: Read data Json to Request
     // TODO: Create file if not exists
     // TODO: 6/19/21 append the error in the file
     // TODO: 6/19/21 Response with ok is all successful
@@ -12,8 +16,20 @@ public class CreateMessageController implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) {
         System.out.println(exchange.getRequestMethod());
-        System.out.println(exchange.getRequestHeaders());
-        System.out.println(exchange.getResponseBody());
+        InputStream body = exchange.getRequestBody();
+        try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(body))) {
+
+            StringBuilder request = new StringBuilder();
+            String responseLine;
+            while ((responseLine = reader.readLine()) != null) {
+                request.append(responseLine.trim());
+            }
+
+            System.out.println(request);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
 
