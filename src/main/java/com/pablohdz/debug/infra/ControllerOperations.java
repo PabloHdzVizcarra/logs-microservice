09113f34d9a99951ce308294924824431282b21a
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-21 07:28:22 -0500

    feat handler to operations in controller

---
+++ src/main/java/com/pablohdz/debug/infra/ControllerOperations.java
@@ -0,0 +1,26 @@
+package com.pablohdz.debug.infra;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerOperations {
+    public ControllerOperations() {
+    }
+
+    public Map<String, String> queryToMap(String query) {
+        if (query == null) {
+            return null;
+        }
+
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+           String[] entry = param.split("=");
+           if (entry.length > 1) {
+               result.put(entry[0], entry[1]);
+           } else {
+               result.put(entry[0], "");
+           }
+        }
+        return result;
+    }
+}
