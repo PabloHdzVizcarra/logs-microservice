b2c69b0dbf4915a148dfd0ffe61dca5859f21ed3
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-21 07:29:20 -0500

    feat get query in request

--- src/main/java/com/pablohdz/debug/infra/GetMessagesController.java
+++ src/main/java/com/pablohdz/debug/infra/GetMessagesController.java
@@ -4,16 +4,26 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class GetMessagesController implements HttpHandler {
+    // TODO: 6/21/21 get name of file of query parameter
+    // TODO: 6/21/21 search file if exists
+    // TODO: 6/21/21 if not exists send error response
+    // TODO: 6/21/21 create a data structure with all records in file
+    // TODO: 6/21/21 send records to client
     private final ControllersResponse response;
+    private final ControllerOperations operations;
 
     public GetMessagesController(ControllersResponse response) {
         this.response = response;
+        this.operations = new ControllerOperations();
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        Map<String, String> querys = operations.queryToMap(exchange.getRequestURI().getQuery());
+        System.out.println(querys);
         response.withText(exchange, 201);
     }
 }
