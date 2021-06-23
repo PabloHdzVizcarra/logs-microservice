3d4b6eb234f42d2cd5ca9e1b6d7ae976b381b5be
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-22 20:57:32 -0500

    feat modified to read env variables

--- src/main/java/com/pablohdz/debug/DebugMicroservice.java
+++ src/main/java/com/pablohdz/debug/DebugMicroservice.java
@@ -6,30 +6,40 @@ import com.pablohdz.debug.infra.CreateMessageController;
 import com.pablohdz.debug.infra.GetMessagesController;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Map;
 
-public class DebugMicroservice {
-    public static void main(String[] args) {
-        try {
-            HttpServer server = createServer();
+public class DebugMicroservice
+{
+    public static void main(String[] args)
+    {
+
+        try
+        {
+            Map<String, String> env = readEnv();
+            HttpServer server = HttpServer.create(
+                new InetSocketAddress(Integer.parseInt(env.get("PORT"))), 0);
+
             server.createContext(
                 "/api/v1/create",
                 new CreateMessageController(new FileUseCases(), new ControllersResponse()));
+
             server.createContext(
                 "/api/v1/records",
                 new GetMessagesController(new ControllersResponse()));
+
             server.setExecutor(null);
             server.start();
-            System.out.println("Server is on in port 8080");
+            System.out.println("Server is on in port " + env.get("PORT"));
 
-        } catch (Exception exception) {
+        } catch (Exception exception)
+        {
             System.out.println(exception.getMessage());
         }
     }
 
-    public static HttpServer createServer() throws IOException {
-        return HttpServer
-            .create(new InetSocketAddress(8080), 0);
+    public static Map<String, String> readEnv()
+    {
+        return System.getenv();
     }
 }
